cdab296f77ebe7cf5a85514523f07486048d7183
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2016-11-18 17:46:04 +0800

    Multiwindows

---
+++ Multiwindows 18112016/Multiwindows 18112016/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Multiwindows_18112016
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void click(object sender, RoutedEventArgs e)
+        {
+            SecondWindows win2 = new SecondWindows();
+            win2.Owner = this;
+            win2.Show();
+        }
+    }
+}
